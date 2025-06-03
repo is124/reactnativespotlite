f2615ec74410ba036e07fab22ab5d3be10802082
is124 <tilaks484@gmai.com>
2025-06-03 16:42:16 -0700

    maps integrated

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MapboxGL from '@rnmapbox/maps';
 
-MapboxGL.setAccessToken('pk.eyJ1IjoiYXJjb2RlbWFwYm94IiwiYSI6ImNtYmgyNHg2MjA1YXAyanByNG0yNXU3YjIifQ.OiU5zUJcOJ-3GtclIBYN-Q');
+MapboxGL.setAccessToken('');
 
 export default function App() {
   return (
